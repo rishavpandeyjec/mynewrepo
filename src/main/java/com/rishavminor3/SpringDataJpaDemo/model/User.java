e15a230d380e9848dd80acd3957c38c91212407e
[D[Drvpandey <rishavpandeyjec@gmail.com>
2019-06-22 10:38:30 +0530

    Used annotations for getters setters tostring id entity constructor

--- src/main/java/com/rishavminor3/SpringDataJpaDemo/model/User.java
+++ src/main/java/com/rishavminor3/SpringDataJpaDemo/model/User.java
@@ -1,54 +1,29 @@
 package com.rishavminor3.SpringDataJpaDemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class User
 {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
+    @Getter @Setter
     private Long id;
-     private String firstName;
-     private String lastName;
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+    @Getter @Setter
+    private String firstName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
+    @Getter @Setter
+    private String lastName;
+    
 }
